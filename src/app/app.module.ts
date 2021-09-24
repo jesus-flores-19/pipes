a51b3d2403f496bf0c76839fcf1fe30a6f7170cc
Jesus Arath <jesus.flores.p19@gmail.com>
2021-09-23 21:46:05 -0500

    Cambiamos el idioma local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData} from '@angular/common'
+import localEs from '@angular/common/locales/es'
 
 import { AppComponent } from './app.component';
+
+registerLocaleData(localEs);
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+    provide: LOCALE_ID,
+    useValue: "es"
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
